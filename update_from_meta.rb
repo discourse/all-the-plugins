e06998d0194a0e43a2f0f9660c33440176ebf213
Jarek Radosz <jradosz@gmail.com>
2025-02-18 23:13:48 +0100

    Fix the update_from_meta script

--- update_from_meta.rb
+++ update_from_meta.rb
@@ -1,47 +1,41 @@
-require 'json'
-require 'open-uri'
+require "json"
+require "open-uri"
 
-$existing_modules = File.read('plugins.txt')
-                        .split("\n")
-                        .filter{ |str| str.size > 0 }
-                        .compact
+$existing_plugins =
+  File.read("third-party.txt").split("\n").compact + File.read("official.txt").split("\n").compact
+$list = []
 
 def ensure_repo(title, topic_url)
   return if title =~ /about the plugin category/i
 
-  result = open(topic_url).read
+  result = URI.open(topic_url).read
   json = JSON.parse(result)
 
   op_body = json["post_stream"]["posts"][0]["cooked"]
 
-  first_github = nil
-  op_body.scan(/href=['"](http[^'"]+github[^'"]+)/i) do
-    first_github = $1
-    unless first_github =~ /issuecomment/
-      break
-    end
-  end
+  match = op_body.scan(%r{https://github\.com/[A-Z0-9_\-]+/[A-Z0-9_\-]+}i)
+  github_url = match && match[-1]
 
-  if first_github
-    if $existing_modules.any?{|m| first_github.include?(m)}
-      return
-    end
-  end
+  return if !github_url
+  return if $existing_plugins.any? { |m| github_url.include?(m) }
 
-  if first_github
-    puts "Add #{title}: #{path} (Y/n)"
-    text = readline
-    unless text =~ /n/
-      File.write("plugins.txt", "#{path}\n", mode: 'a+')
-      puts "Added #{path}"
-    end
-  end
+  repo = github_url.sub("https://github.com/", "")
 
+  puts title
+  puts topic_url.sub(".json", "")
+  puts github_url
+  print "Add? (Y/n) "
+  text = readline
+
+  $list << repo if text =~ /n/
+
+  puts "\n"
 end
 
-url = 'https://meta.discourse.org/c/plugin/none.json'
+url = "https://meta.discourse.org/c/plugin/none.json"
+
 begin
-  result = open(url).read
+  result = URI.open(url).read
   json = JSON.parse(result)
 
   topic_list = json["topic_list"]
@@ -57,3 +51,5 @@ begin
   url = "https://meta.discourse.org#{topic_list["more_topics_url"]}"
   url = url.sub("latest", "latest.json")
 end while topics.length > 0
+
+puts $list
